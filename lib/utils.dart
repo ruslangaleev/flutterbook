fc9b65df72dbc9947ca6c12d7aea297f356803ff
Ruslan Galeev <ruslan.nafisovich@gmail.com>
2021-09-02 16:11:55 +0500

    Added Tab widget

---
+++ lib/utils.dart
@@ -0,0 +1,8 @@
+// Содержит глобальные данные
+
+import "dart:io";
+import "package:flutter/material.dart";
+import "package:path_provider/path_provider.dart";
+import "package:intl/intl.dart";
+
+Directory docsDir = Directory("/data/user/0/com.example.flutterbook/app_flutter");
